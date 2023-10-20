7c31842db7513775815cb996ef2ccdb4fcbb11b9
DiegoGutterres <diegogfui123@gmail.com>
2023-10-20 12:02:20 -0300

    base

---
+++ auto.py
@@ -0,0 +1,88 @@
+import time
+import pandas as pd
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from config import CHROME_PROFILE_PATH
+from datetime import date
+
+#driver
+options = webdriver.ChromeOptions()
+options.add_argument(CHROME_PROFILE_PATH)
+driver = webdriver.Chrome(options=options)
+driver.get("https://app.contaazul.com/#/financeiro/contas-a-receber?view=revenue&amp;source=Financeiro%20%3E%20Contas%20a%20Receber&source=Menu%20Principal")
+
+#inicio da automação
+while True:
+    try:
+        control = driver.find_element(By.XPATH, '//*[@id="statement-list-container"]/table[1]/tbody')
+        if (control):
+            time.sleep(1)
+            break
+    except:
+        time.sleep(2)
+
+#exibir
+driver.find_element(By.XPATH, '//*[@id="type-filter-controller"]/span').click()
+time.sleep(.5)
+
+#recebido
+driver.find_element(By.XPATH, '//*[@id="typeFilterContainer"]/li[4]/a/span[1]').click()
+
+#aplicar
+driver.find_element(By.XPATH, '//*[@id="type-filter"]/ul/li[2]/div/button').click()
+time.sleep(3)
+
+#filtrar contas
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/button').click()
+
+#all
+#listar a ordem e largar num loop
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[1]/a/span[1]').click()
+time.sleep(0.3)
+
+#bradesco
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[2]/a/span').click()
+
+#itau
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[3]/a/span').click()
+
+#sap
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[4]/a/span').click()
+
+#sicredi
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[5]/a/span').click()
+
+#nova data
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[16]/a/span').click()
+
+#cartao iugu
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[35]/a/span').click()
+
+#aplicar
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[3]/div/button').click()
+time.sleep(3)
+ 
+#ir para cima
+driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.CONTROL, Keys.HOME)
+time.sleep(1)
+
+#filtrar data
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/button/span').click()
+
+#hoje
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/ul/li[1]/a').click()
+time.sleep(3)
+
+#ir para dois dias a frente
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[4]/a[2]/i').click()
+time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[4]/a[2]/i').click()
+time.sleep(1)
+
+#pesquisar
+def search(cliente):
+    driver.find_element(By.XPATH, '//*[@id="textSearch"]').send_keys(cliente)
+        
---
+++ config.py
@@ -0,0 +1,1 @@
+CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\victor.ludwig_v4comp\\AppData\\Local\\Google\\Chrome\\User Data\\ContaAzul"
